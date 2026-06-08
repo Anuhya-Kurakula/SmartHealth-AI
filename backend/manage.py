e02b734314c59abc3c6f7b048aa5c6a4d57fd7a0
Anuhya Kurakula <anuhyakurakula@gmail.com>
2026-06-08 11:16:54 +0530

    Fixed Python path for Render

--- backend/manage.py
+++ backend/manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 from pathlib import Path
+from django.core.management import execute_from_command_line
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
