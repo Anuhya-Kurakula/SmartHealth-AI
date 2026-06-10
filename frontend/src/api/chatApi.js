249c119e16730b0c24669e7529fbab9f15d01b4e
Anuhya Kurakula <anuhyakurakula@gmail.com>
2026-06-10 18:27:17 +0530

    Rollback production settings

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,9 +1,7 @@
 export const getChatResponse = async (question) => {
-
   const response = await fetch(
     `${import.meta.env.VITE_API_URL}/api/chat/?question=${encodeURIComponent(question)}`
   );
 
   return await response.json();
-
 };
